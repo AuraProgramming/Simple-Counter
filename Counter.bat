4c853b28a0463d7be683e02fc75080318d2d7607
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-06-03 15:30:19 -0400

    Update Counter.bat

--- Counter.bat
+++ Counter.bat
@@ -1,12 +1,19 @@
 @echo off
-  
+
+title Counter
+
 cls
 
 set /a e=0
+set /a a=0
+set /a g=0
 
 :count
 timeout /t 1 > nul
 cls
 set /a e=%e%+"1"
-echo %e%
+if "%e%"=="60" set /a a="%a%"+"1"
+if "%a%"=="60" set /a g="%a%"+"1"
+echo %g% hours %a% minutes %e% seconds
 goto count
+:countminute
